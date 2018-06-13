c779197ad537979ac512c3b5c8b2e387e3f16eac
TrainingWheelsAdmin <40239071+TrainingWheelsAdmin@users.noreply.github.com>
2018-06-13 20:02:29 +0530

    Update AdminTableAlterScripts_V1.sql

--- TrainingAppScripts/AdminTableAlterScripts_V1.sql
+++ TrainingAppScripts/AdminTableAlterScripts_V1.sql
@@ -1,3 +1,6 @@
+USE TrainingWheelsAdminDb
+GO
+
 IF COL_LENGTH('schemaName.Carbon.TenantAddress', 'TenantAddress_Status') IS NULL
 BEGIN
     ALTER TABLE Carbon.TenantAddress ADD TenantAddress_Status BIT NOT NULL
